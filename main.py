90a9166ec99c7b390979924a5cab19a0e4e876a4
Joseph Bacal <bacalj@gmail.com>
2023-03-27 11:12:35 -0400

    simplified for first story

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-def on_button_pressed_a():
-    basic.show_number(dht11_dht22.read_data(dataType.TEMPERATURE))
-input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_ab():
+    global hubIndex, currentHub
+    if hubIndex < 4:
+        hubIndex = hubIndex + 1
+    else:
+        hubIndex = 0
+    currentHub = hubs[hubIndex]
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-def on_button_pressed_b():
-    basic.show_icon(IconNames.HEART)
-input.on_button_pressed(Button.B, on_button_pressed_b)
+currentHub = ""
+hubIndex = 0
+hubs: List[str] = []
+radio.set_group(1)
+hubs = ["x", "a", "b", "c", "d"]
+hubIndex = 0
+currentHub = hubs[hubIndex]
 
 def on_forever():
+    basic.show_string("" + (currentHub))
     dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P1, True, False, True)
+    radio.send_string("" + currentHub + str(dht11_dht22.read_data(dataType.TEMPERATURE)))
+    basic.show_number(dht11_dht22.read_data(dataType.TEMPERATURE))
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,26 +1,4 @@
-input.onButtonPressed(Button.AB, function () {
-    if (hubIndex < 4) {
-        hubIndex = hubIndex + 1
-    } else {
-        hubIndex = 0
-    }
-    currentHub = hubs[hubIndex]
-})
-let currentHub = ""
-let hubIndex = 0
-let hubs: string[] = []
-radio.setGroup(1)
-hubs = [
-"x",
-"a",
-"b",
-"c",
-"d"
-]
-hubIndex = 0
-currentHub = hubs[hubIndex]
-basic.forever(function () {
-    basic.showString("" + (currentHub))
+function readTempAndSend () {
     dht11_dht22.queryData(
     DHTtype.DHT11,
     DigitalPin.P1,
@@ -28,6 +6,20 @@ basic.forever(function () {
     false,
     true
     )
-    radio.sendString("" + currentHub + dht11_dht22.readData(dataType.temperature))
-    basic.showNumber(dht11_dht22.readData(dataType.temperature))
+    tempReading = dht11_dht22.readData(dataType.temperature)
+    if (tempReading > 0) {
+        packetString = "s" + hubIdentifier + "t" + tempReading
+    } else {
+        packetString = "s" + hubIdentifier + "t" + "empty"
+    }
+    radio.sendString(packetString)
+    basic.showString(packetString)
+}
+let packetString = ""
+let tempReading = 0
+let hubIdentifier = ""
+radio.setGroup(1)
+hubIdentifier = "a"
+basic.forever(function () {
+    readTempAndSend()
 })
